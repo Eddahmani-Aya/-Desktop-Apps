c6382770bfc9dc45d4a38c172619cfc610e8785d
Eddahmani Aya <edaya816@gmail.com>
2026-01-19 23:22:55 +0100

    🖥 C# Desktop Apps

---
+++ tp_13/TP_12/Program.cs
@@ -0,0 +1,107 @@
+﻿class program
+{
+    static void Main()
+    {
+        //Exercice 1:
+        /*int[] T = new int[3];
+        T[0] = 1;
+        T[1] = 22;
+        T[2] = 33; 
+        for (int i = 0; i < T.Length; i++)
+        {
+            Console.WriteLine("T[" + i + "] = " + T[i]);
+        }
+        //Exercice 2:
+        float[] T = new float[5];
+        for(int i=0;i<T.Length;i++)
+        {
+            Console.Write("Entrez valeur de T[" + i + "] :");
+            T[i]=int.Parse(Console.ReadLine());
+        }
+        for (int i = 0; i < T.Length; i++)
+        {
+            Console.WriteLine("T[" + i + "] : "+ T[i] );
+        }
+        //Exercice 3 :
+        float[] T = new float[5];
+        remplire(T);
+        affichage(T);
+        float somme = calculerSomme(T);
+        float moyenne = somme / T.Length;
+        float produit = calculerProduit(T);
+        Console.WriteLine("Somme = " + somme);
+        Console.WriteLine("Moyenne = " + moyenne);
+        Console.WriteLine("Produit = " + produit);
+    }
+        static void remplire(float[] T)
+        {
+            for (int i = 0; i < T.Length; i++)
+            {
+                Console.Write("Entrez valeur de T[" + i + "] :");
+                T[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static void affichage(float[] T)
+        {
+            for (int i = 0; i < T.Length; i++)
+            {
+                Console.WriteLine("T[" + i + "] : " + T[i]);
+            }
+        }
+        static float calculerSomme(float[] T)
+        {
+            float somme = 0;
+            for (int i = 0; i < T.Length; i++)
+            {
+                somme += T[i];
+            }
+            return somme;
+        }
+        static float calculerProduit(float[] T)
+        {
+            float produit = 1;
+            for (int i = 0; i < T.Length; i++)
+            {
+                produit *= T[i];
+            }
+            return produit;
+        }
+        //Exercice 4 :
+        int[] T = new int[5];
+            remplire(T);
+            affichage(T);
+            int max = T[0];
+            int min = T[0];
+
+            // Recherche du max et du min
+            for (int i = 1; i < T.Length; i++)
+            {
+                if (T[i] > max)
+                    max = T[i];
+                if (T[i] < min)
+                    min = T[i];
+            }
+
+            // Affichage du résultat
+            Console.WriteLine("Valeur maximale = " + max);
+            Console.WriteLine("Valeur minimale = " + min);
+        static void remplire(int[] T)
+            {
+                for (int i = 0; i < T.Length; i++)
+                {
+                    Console.Write("Entrez valeur de T[" + i + "] :");
+                    T[i] = int.Parse(Console.ReadLine());
+                }
+            }
+            static void affichage(int[] T)
+            {
+                for (int i = 0; i < T.Length; i++)
+                {
+                    Console.WriteLine("T[" + i + "] : " + T[i]);
+                }
+            }*/
+            //Exercice 5 :
+
+    }
+}
+
