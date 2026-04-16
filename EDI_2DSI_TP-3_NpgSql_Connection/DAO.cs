c79658ad928fb5b0e3569231d4c79cdf06da3600
Eddahmani Aya <edaya816@gmail.com>
2026-04-17 01:46:37 +0200

    🖥 C# Desktop Apps(AdoNet)

---
+++ EDI_2DSI_TP-3_NpgSql_Connection/DAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED_2DAI_TP_2
+{
+    internal class DAO
+    {
+
+        public DAO()
+        {
+         
+        }
+
+
+
+    }
+}
---
+++ EDI_2DSI_TP-3_NpgSql_Connection/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ED_2DAI_TP_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Connexion = new System.Windows.Forms.Button();
+            this.btn_DeConnexion = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_Etat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(176, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BDD PostGre";
+            // 
+            // btn_Connexion
+            // 
+            this.btn_Connexion.Font = new System.Drawing.Font("Trebuchet MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Connexion.Location = new System.Drawing.Point(90, 131);
+            this.btn_Connexion.Name = "btn_Connexion";
+            this.btn_Connexion.Size = new System.Drawing.Size(115, 36);
+            this.btn_Connexion.TabIndex = 1;
+            this.btn_Connexion.Text = "Connexion";
+            this.btn_Connexion.UseVisualStyleBackColor = true;
+            // 
+            // btn_DeConnexion
+            // 
+            this.btn_DeConnexion.Font = new System.Drawing.Font("Trebuchet MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DeConnexion.Location = new System.Drawing.Point(263, 131);
+            this.btn_DeConnexion.Name = "btn_DeConnexion";
+            this.btn_DeConnexion.Size = new System.Drawing.Size(115, 36);
+            this.btn_DeConnexion.TabIndex = 2;
+            this.btn_DeConnexion.Text = "Deconnexion";
+            this.btn_DeConnexion.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(85, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Etat";
+            // 
+            // lbl_Etat
+            // 
+            this.lbl_Etat.AutoSize = true;
+            this.lbl_Etat.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Etat.Location = new System.Drawing.Point(258, 210);
+            this.lbl_Etat.Name = "lbl_Etat";
+            this.lbl_Etat.Size = new System.Drawing.Size(133, 27);
+            this.lbl_Etat.TabIndex = 4;
+            this.lbl_Etat.Text = "Diconnected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 308);
+            this.Controls.Add(this.lbl_Etat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_DeConnexion);
+            this.Controls.Add(this.btn_Connexion);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Connexion;
+        private System.Windows.Forms.Button btn_DeConnexion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_Etat;
+    }
+}
+
