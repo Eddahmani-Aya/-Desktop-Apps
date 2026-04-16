1c8482fabbe2d2c3d07a03dec9381760a92ca30a
Eddahmani Aya <edaya816@gmail.com>
2026-04-17 01:52:08 +0200

    🖥 C# Desktop Apps(AdoNet)

---
+++ EDI_2DSI_TP-7_SELECT-LBL/DAO.cs
@@ -0,0 +1,53 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ED_2DAI_TP_6_Correction
+{
+    public class DAO
+    {
+        public NpgsqlConnection cnx;
+        public NpgsqlCommand cmd;
+        public void connexion(String req)
+        {
+            try
+            {
+                cnx = new NpgsqlConnection(req);
+                if (cnx.State==0)
+                {
+                    cnx.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+        public String readData(String req)
+        {
+            String data = "";
+            try
+            {
+                cmd = new NpgsqlCommand(req, cnx);
+                NpgsqlDataReader dr =cmd.ExecuteReader();   
+                while (dr.Read())
+                {
+                    data += dr[0].ToString() + "\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return data;
+        }
+    } 
+}
+
---
+++ EDI_2DSI_TP-7_SELECT-LBL/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ED_2DAI_TP_6_Correction
+{
+    public partial class Form1 : Form
+    {
+        public DAO x;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            x = new DAO();
+            x.connexion("server=localhost;database=BDD_BOOKS;userid=postgres;password=AYATON223");
+        }
+
+        private void btn_Check_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string id = txtBx_ID.Text;
+                string query = $"SELECT auteur FROM livre WHERE id = {id}";
+                lbl_Auteur.Text = x.readData(query);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
