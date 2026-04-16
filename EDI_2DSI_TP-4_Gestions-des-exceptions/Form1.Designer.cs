819e9cd24d80bf05b17c7e4f2b99a302a372483f
Eddahmani Aya <edaya816@gmail.com>
2026-04-17 01:47:41 +0200

    🖥 C# Desktop Apps(AdoNet)

---
+++ EDI_2DSI_TP-4_Gestions-des-exceptions/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace ED_2DAI_TP_4_Correction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBx_Indice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Indice = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_BDD = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBx_BDD = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btn_Octet = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtBx_Octet = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btn_Nbr = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtBx_Nbr = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(435, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TP-4 Gestion des exception";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(18, 172);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(310, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IndexOutOfRangeException";
+            // 
+            // txtBx_Indice
+            // 
+            this.txtBx_Indice.Location = new System.Drawing.Point(357, 177);
+            this.txtBx_Indice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBx_Indice.Name = "txtBx_Indice";
+            this.txtBx_Indice.Size = new System.Drawing.Size(148, 26);
+            this.txtBx_Indice.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(399, 138);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 28);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Indice";
+            // 
+            // btn_Indice
+            // 
+            this.btn_Indice.Font = new System.Drawing.Font("Trebuchet MS", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Indice.Location = new System.Drawing.Point(567, 160);
+            this.btn_Indice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Indice.Name = "btn_Indice";
+            this.btn_Indice.Size = new System.Drawing.Size(351, 66);
+            this.btn_Indice.TabIndex = 4;
+            this.btn_Indice.Text = "Afficher l\'element (indice) du tableau";
+            this.btn_Indice.UseVisualStyleBackColor = true;
+            this.btn_Indice.Click += new System.EventHandler(this.btn_Indice_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(24, 248);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(894, 2);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(24, 377);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(894, 2);
+            this.label5.TabIndex = 10;
+            // 
+            // btn_BDD
+            // 
+            this.btn_BDD.Font = new System.Drawing.Font("Trebuchet MS", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_BDD.Location = new System.Drawing.Point(567, 289);
+            this.btn_BDD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_BDD.Name = "btn_BDD";
+            this.btn_BDD.Size = new System.Drawing.Size(351, 66);
+            this.btn_BDD.TabIndex = 9;
+            this.btn_BDD.Text = "Connexion avec la BDD";
+            this.btn_BDD.UseVisualStyleBackColor = true;
+            this.btn_BDD.Click += new System.EventHandler(this.btn_BDD_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(375, 268);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 28);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "BDD Name";
+            // 
+            // txtBx_BDD
+            // 
+            this.txtBx_BDD.Location = new System.Drawing.Point(357, 306);
+            this.txtBx_BDD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBx_BDD.Name = "txtBx_BDD";
+            this.txtBx_BDD.Size = new System.Drawing.Size(148, 26);
+            this.txtBx_BDD.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(18, 302);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(195, 28);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "NpgSqlException";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Black;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label8.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(24, 508);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(894, 2);
+            this.label8.TabIndex = 15;
+            // 
+            // btn_Octet
+            // 
+            this.btn_Octet.Font = new System.Drawing.Font("Trebuchet MS", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Octet.Location = new System.Drawing.Point(567, 420);
+            this.btn_Octet.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Octet.Name = "btn_Octet";
+            this.btn_Octet.Size = new System.Drawing.Size(351, 66);
+            this.btn_Octet.TabIndex = 14;
+            this.btn_Octet.Text = "Afficher l\'octet saisi";
+            this.btn_Octet.UseVisualStyleBackColor = true;
+            this.btn_Octet.Click += new System.EventHandler(this.btn_Octet_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(380, 398);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(92, 28);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "1 Octet";
+            // 
+            // txtBx_Octet
+            // 
+            this.txtBx_Octet.Location = new System.Drawing.Point(357, 437);
+            this.txtBx_Octet.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBx_Octet.Name = "txtBx_Octet";
+            this.txtBx_Octet.Size = new System.Drawing.Size(148, 26);
+            this.txtBx_Octet.TabIndex = 12;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(18, 432);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(224, 28);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "OverFlowException";
+            // 
+            // btn_Nbr
+            // 
+            this.btn_Nbr.Font = new System.Drawing.Font("Trebuchet MS", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Nbr.Location = new System.Drawing.Point(567, 558);
+            this.btn_Nbr.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Nbr.Name = "btn_Nbr";
+            this.btn_Nbr.Size = new System.Drawing.Size(351, 66);
+            this.btn_Nbr.TabIndex = 19;
+            this.btn_Nbr.Text = "Afficher le nombre saisi";
+            this.btn_Nbr.UseVisualStyleBackColor = true;
+            this.btn_Nbr.Click += new System.EventHandler(this.btn_Nbr_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.Black;
+            this.label12.Location = new System.Drawing.Point(381, 537);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(96, 28);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "Nombre";
+            // 
+            // txtBx_Nbr
+            // 
+            this.txtBx_Nbr.Location = new System.Drawing.Point(357, 575);
+            this.txtBx_Nbr.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBx_Nbr.Name = "txtBx_Nbr";
+            this.txtBx_Nbr.Size = new System.Drawing.Size(148, 26);
+            this.txtBx_Nbr.TabIndex = 17;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.Red;
+            this.label13.Location = new System.Drawing.Point(18, 571);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(200, 28);
+            this.label13.TabIndex = 16;
+            this.label13.Text = "FormatException";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 682);
+            this.Controls.Add(this.btn_Nbr);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtBx_Nbr);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btn_Octet);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtBx_Octet);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_BDD);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtBx_BDD);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_Indice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBx_Indice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBx_Indice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Indice;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_BDD;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBx_BDD;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btn_Octet;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtBx_Octet;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btn_Nbr;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtBx_Nbr;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ EDI_2DSI_TP-4_Gestions-des-exceptions/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Npgsql;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ED_2DAI_TP_4_Correction
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Indice_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int[] T = { 1, 4567, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 };
+                int indice = int.Parse(txtBx_Indice.Text);
+                MessageBox.Show("Nbr : " + T[indice]);
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+            
+        }
+
+        private void btn_BDD_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String db = txtBx_BDD.Text;
+                NpgsqlConnection conn = new NpgsqlConnection("Host=localhost;database=" + db+ ";Username=postgres;password=AYATON223");
+                conn.Open();
+                MessageBox.Show("Connexion réussie");
+
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+            
+        }
+
+        private void btn_Octet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte x = byte.Parse(txtBx_Octet.Text);
+                MessageBox.Show("la valeur de cette variable est"+x);
+            }
+            catch(OverflowException ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+
+        private void btn_Nbr_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int x = int.Parse(txtBx_Nbr.Text);
+                MessageBox.Show("la valeur de cette variable est" + x);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+    }
+}
